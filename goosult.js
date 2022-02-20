fc641fb09326e852a0d945ec9579308273b9b978
Unzor <71789103+Unzor@users.noreply.github.com>
2022-02-20 10:13:44 -0600

    Update goosult.js

--- goosult.js
+++ goosult.js
@@ -7,12 +7,15 @@ var dom = new DOMParser().parseFromString(page, "text/html")
 var e2 = [];
 
 var all_elements = Array.from(dom.querySelectorAll("h3")).map(x => x.innerHTML.split(">")[1].split("<")[0]);
+var all_element_links = Array.from(dom.querySelectorAll("h3")).map(x => x.parentElement.href);
 
-all_elements.forEach(function(en){
-  if (!en.startsWith("<span ") && !en.endsWith("</span>") && en !== "Videos" && en !== "Images" && en !== "Top stories" && en !== "") {
-    e2.push(en);
+
+all_elements.forEach(function(en, index){
+  if (!en.startsWith("<span ") && !en.endsWith("</span>") && en !== "Videos" && en !== "Images" && en !== "Top stories" && en !== "" && all_element_links[index]) {
+    e2.push({header: en, link: decodeURIComponent(all_element_links[index].split("?q=").pop().split("&").shift())});
   }
 })
+
 
 return e2;
 }
